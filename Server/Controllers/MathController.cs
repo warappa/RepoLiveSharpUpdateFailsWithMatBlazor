f33e5e14d94ccbd43289deae14c82829066f6fb8
warappa <spoiltnr1@gmx.at>
2020-11-24 21:15:11 +0100

    LiveSharp 1.6.33

---
+++ Server/Controllers/MathController.cs
@@ -0,0 +1,29 @@
+﻿using RepoLiveSharpUpdateFailsWithMatBlazor.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RepoLiveSharpUpdateFailsWithMatBlazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MathController : ControllerBase
+    {
+        
+        private readonly ILogger<MathController> logger;
+
+        public MathController(ILogger<MathController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(int a, int b)
+        {
+            return $"{a} - {b} = {a + b}";
+        }
+    }
+}
